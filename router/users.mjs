87f142f89b6caa9b8405cf2fc7ec32c9b578205a
Ashutosh <ashutosh.pai@hebbalelabs.com>
2022-08-05 11:04:56 +0530

    Updated with axios.

--- router/users.mjs
+++ router/users.mjs
@@ -1,23 +1,39 @@
 import express from "express";
-import https from "https";
+import axios from "axios";
 
 const Router = express.Router();
 
-Router.get("/", (req, res)=>{
-    https.get({
-        protocol : "https:",
-        hostname : "jsonplaceholder.typicode.com",
-        path : "/users"
-    }, (response)=>{
-        let data = "";
-        response.on("data", (chunk)=>{
-            data = data + chunk;
-        });
-
-        response.on("close", ()=>{
-            res.send(JSON.parse(data));
-        })
-    });
+Router.get("/", async (req, res)=>{
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send("Internal server error");
+    }
 })
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+// W/O azios
+// import https from "https";
+
+// const Router = express.Router();
+
+// Router.get("/", (req, res)=>{
+//     https.get({
+//         protocol : "https:",
+//         hostname : "jsonplaceholder.typicode.com",
+//         path : "/users"
+//     }, (response)=>{
+//         let data = "";
+//         response.on("data", (chunk)=>{
+//             data = data + chunk;
+//         });
+
+//         response.on("close", ()=>{
+//             res.send(JSON.parse(data));
+//         })
+//     });
+// })
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 export default Router;
