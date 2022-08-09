60a873993f24c0880b9a9e7afe2e2684f2ebcbf0
Ashutosh <ashutosh.pai@hebbalelabs.com>
2022-08-09 11:34:14 +0530

    cleanup for api methods.

--- router/productsDB.mjs
+++ router/productsDB.mjs
@@ -5,87 +5,117 @@ import { createClient } from "../utils/db.mjs";
 const Router = express.Router();
 
 // GET
-Router.get("/", async (req, res)=>{
-    const client = createClient();
-    await client.connect();
-    const db = client.db("nodeJsTraining");
-    const collection = db.collection("products");
-    const docs = await collection.find().toArray();
-    client.close();
+Router.get("/", async (req, res) => {
+    try {
+        const client = createClient();
+        await client.connect();
+        const db = client.db("nodeJsTraining");
+        const collection = db.collection("products");
+        const docs = await collection.find().toArray();
+        client.close();
 
-    res.json(docs);
+        res.json(docs);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 })
 
 // GET with ID
-Router.get("/:id", async (req, res)=>{
-    const id = req.params.id;
+Router.get("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
 
-    const client = createClient();
-    await client.connect();
-    const db = client.db("nodeJsTraining");
-    const collection = db.collection("products");
-    const doc = await collection.find({_id : ObjectID(id)}).toArray();
-    client.close();
+        const client = createClient();
+        await client.connect();
+        const db = client.db("nodeJsTraining");
+        const collection = db.collection("products");
+        const doc = await collection.find({ _id: ObjectID(id) }).toArray();
+        client.close();
 
-    res.json(doc);
+        res.json(doc);
+
+        if (doc === null) {
+            res.status(404).json({ message: "product not found." });
+        } else {
+            res.json(doc);
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error." });
+    }
 })
 
 
 // POST
-Router.post("/", async (req, res)=>{
-    const body = req.body;
+Router.post("/", async (req, res) => {
+    try {
+        const body = req.body;
 
-    const client = createClient();
-    await client.connect();
-    const db = client.db("nodeJsTraining");
-    const collection = db.collection("products");
-    const insertDocs = await collection.insertMany(body);
-    client.close();
+        const client = createClient();
+        await client.connect();
+        const db = client.db("nodeJsTraining");
+        const collection = db.collection("products");
+        await collection.insertMany(body);
+        client.close();
 
-    res.json(insertDocs);
+        res.json({ ...body, _id: insertedDoc.insertedId });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error." });
+    }
 })
 
 // PUT
-Router.put("/:id", async (req, res)=>{
-    const id = req.params.id;
-    const body = req.body;
+Router.put("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const body = req.body;
 
-    const client = createClient();
-    await client.connect();
-    const db = client.db("nodeJsTraining");
-    const collection = db.collection("products");
-    const updateDoc = await collection.updateOne({_id : ObjectID(id)}, {$set : body});
-    client.close();
+        const client = createClient();
+        await client.connect();
+        const db = client.db("nodeJsTraining");
+        const collection = db.collection("products");
+        await collection.updateOne({ _id: ObjectID(id) }, { $set: body });
+        client.close();
 
-    res.json(updateDoc);
+        res.json({ message: "product updated successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error." });
+    }
 })
 
 // PUT with new field to all doc
-Router.put("/", async (req, res)=>{
-    const body = req.body;
+Router.put("/", async (req, res) => {
+    try {
+        const body = req.body;
 
-    const client = createClient();
-    await client.connect();
-    const db = client.db("nodeJsTraining");
-    const collection = db.collection("products");
-    const updateDoc = await collection.updateMany({}, {$set : body});
-    client.close();
+        const client = createClient();
+        await client.connect();
+        const db = client.db("nodeJsTraining");
+        const collection = db.collection("products");
+        await collection.updateMany({}, { $set: body });
+        client.close();
 
-    res.json(updateDoc);
+        res.json({ message: "products updated successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error." });
+    }
 })
 
 // Delete with ID
-Router.delete("/:id", async (req, res)=>{
-    const id = req.params.id;
+Router.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
 
-    const client = createClient();
-    await client.connect();
-    const db = client.db("nodeJsTraining");
-    const collection = db.collection("products");
-    const doc = await collection.deleteOne({_id : ObjectID(id)});
-    client.close();
+        const client = createClient();
+        await client.connect();
+        const db = client.db("nodeJsTraining");
+        const collection = db.collection("products");
+        await collection.deleteOne({ _id: ObjectID(id) });
+        client.close();
 
-    res.json(doc);
+        res.json({ message: "Product deleted succesfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 })
 
 export default Router;
